457fdaf3a4563b6f3c1d4a4369a5c4eeebcc1e3b
SHIRISHNALAWADE <nalawadeshirish5465@gmail.com>
2023-10-21 14:28:10 +0530

    done

---
+++ student.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<student.h>
+using namespace std;
+ class student
+ {
+ 	private int studentId,marks;
+ 	private string name;
+ 	
+ 	student(int studentId,string name,int marks)
+ 	{
+ 		this->studentId=studentId;
+ 		this->marks=marks;
+ 		this->name=name;
+	 }
+	 
+ }
